c6b587216630de68247e0a378318f29e2bc72dd9
Suyalvivek <suyal10vivek@gmail.com>
2026-05-08 09:48:12 +0530

    Add calendar events tool and enhance agent functionality with graph state visualization

--- gen-ai/react-agent/agent.js
+++ gen-ai/react-agent/agent.js
@@ -1,11 +1,16 @@
 console.log("Agent working");
-
+import { read, writeFileSync } from "node:fs";
+import readline from "node:readline/promises";
 import { ChatGroq } from "@langchain/groq";
 import { HumanMessage } from "@langchain/core/messages";
 
 import { createAgent } from "langchain";
 
 import { TavilySearch } from "@langchain/tavily";
+import { tool } from "@langchain/core/tools";
+import { z } from "zod";
+import { MemorySaver } from "@langchain/langgraph-checkpoint";
+import { threadId } from "node:worker_threads";
 
 // const message = new HumanMessage("What color is the sky?");
 
@@ -28,24 +33,80 @@ async function main() {
     // includeDomains: [],
     // excludeDomains: [],
   });
+  //CUSTOM TOOL CALLING
+  const calendarEvents = tool(
+    async ({ query }) => {
+      // Custom logic here
+      // return "You have a meeting at 3 PM today.";
+      return JSON.stringify([
+        {
+          title: "Meeting with Yogesh",
+          time: "3 PM",
+          description: "Discuss the project timeline",
+        },
+      ]);
+    },
+    {
+      name: "get-calendar-events",
+      description: "Call to get calendar events for a user",
+      schema: z.object({
+        query: z
+          .string()
+          .describe("The query to be used for calendar event search"),
+      }),
+    },
+  );
+  // 1. Initialize the in-memory checkpointer
+  const checkpointer = new MemorySaver();
 
   const agent = createAgent({
     model: model,
-    tools: [search],
+    tools: [search, calendarEvents],
+    checkpointer: checkpointer,
   });
 
-  const result = await agent.invoke({
-    messages: [
-      {
-        role: "user",
-        content: "What is the current weather in Dwarka Delhi?",
-      },
-    ],
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
+  while (true) {
+    const userQuery = await rl.question("Enter your query: ");
+    if (userQuery === "bye") {
+      console.log("Goodbye!");
+      break;
+    }
+    const result = await agent.invoke({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful assistant.Use the provided tool to get the information if you don't have it. Current Date and time: ${new Date().toUTCString()}",
+        },
 
-  console.log(
-    "Assistant:",
-    result.messages[result.messages.length - 1].content,
-  );
+        {
+          role: "user",
+          // content: "Do i have any meetings today?",
+          content: userQuery,
+        },
+      ],
+    },{
+      configurable:{
+        thread_id:'1',
+      }
+    });
+
+    console.log(
+      "Assistant:",
+      result.messages[result.messages.length - 1].content,
+    );
+  }
+  rl.close();
+
+  const drawableGraphGraphState = await agent.getGraphAsync();
+  const graphStateImage = await drawableGraphGraphState.drawMermaidPng();
+  const graphStateArrayBuffer = await graphStateImage.arrayBuffer();
+
+  const filePath = "./graphState.png";
+  writeFileSync(filePath, new Uint8Array(graphStateArrayBuffer));
 }
 main();
